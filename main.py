6fe0cf47c4efbc38e61f5f537528bbf998e09af1
nishant <npmaharaj1@gmail.com>
2025-04-29 09:52:31 +1000

    Fix bug by including published-print in date entry

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
 import json
 
-def main():
-    jsondata = dict(json.loads(requests.get(input("Insert URL: ")).content)["message"])
-
+def crossref(jsondata):
     # Get Author
     author = dict(jsondata["author"][0])
     print(f"Author: {author["given"]} {author["family"]}")
@@ -11,6 +9,10 @@ def main():
     # Get Article Title
     title = jsondata["title"][0]
     print(f"Article Title: {title}")
+
+    # Journal Title
+    jtitle = jsondata["container-title"][0]
+    print(f"Journal Title: {jtitle}")
 
     # Get Volume
     volume = jsondata["volume"]
@@ -21,8 +23,16 @@ def main():
     print(f"Issue: {issue}")
 
     # Get Publication Date
-    date = jsondata["published-online"]["date-parts"][0]
-    print(f"Published: {date[2]}/{date[1]}/{date[0]}")
+    try:
+        date = jsondata["published-online"]["date-parts"][0]
+    except:
+        date = jsondata["published-print"]["date-parts"][0]
+    if len(date) == 3:
+        print(f"Published: {date[2]}/{date[1]}/{date[0]}")
+    elif len(date) == 2:
+        print(f"Published: {date[1]}/{date[0]}")
+    else:
+        print(f"Published: {date[0]}")
 
     # Get Short Title
     short = jsondata["short-title"]
@@ -31,6 +41,21 @@ def main():
     # Get DOI
     doi = jsondata["DOI"]
     print(f"DOI: {doi}")
+
+
+
+def main():
+    userInput = input("Insert URL or DOI: ")
+
+    if userInput[0:6] == "https://":
+        print("Defaulting to Crossref...")
+        jsondata = dict(json.loads(requests.get(input(userInput)).content)["message"])
+        crossref(jsondata)
+    else:
+        print("Interpreting DOI...")
+        jsondata = dict(json.loads(requests.get(f"https://api.crossref.org/works/{userInput}").content)["message"])
+        crossref(jsondata)
+
 
     # Uncomment to prettify output
     # print(json.dumps(jsondata, indent=4))
